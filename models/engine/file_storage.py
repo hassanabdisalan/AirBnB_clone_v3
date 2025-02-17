ccd6e063b2d5913e2a534c38e49a31b127d14993
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-02-17 16:37:47 +0300

    task 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,7 +44,7 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            json_objects[key] = self.__objects[key].to_dict(save_check=True)
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
@@ -57,7 +57,7 @@ class FileStorage:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
         except:
             pass
-    
+
     def get(self, cls, id):
         """Retrieving object by class and/or id
         """
